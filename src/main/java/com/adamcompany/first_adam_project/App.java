ec32e35a8154748c24ce489cd9214f31f9950570
AyZedAitch <adam.houlihal@usmba.ac.ma>
2023-10-21 15:19:45 +0100

    first commit

---
+++ src/main/java/com/adamcompany/first_adam_project/App.java
@@ -0,0 +1,13 @@
+package com.adamcompany.first_adam_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
